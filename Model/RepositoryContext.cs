821c4f00c216ba6db7e130864c906d9e2395e941
kemal dencik <kemaldencik0163@gmail.com>
2023-11-21 17:09:36 +0300

    Migrations

--- Model/RepositoryContext.cs
+++ Model/RepositoryContext.cs
@@ -9,5 +9,33 @@ namespace StoreApp.Model
         {
             
         }
+        //mthod geçersiz kılma
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product()
+                {
+                    ProductId=1,ProductName="Computer",Price=20_000
+                },
+                new Product()
+                {
+                    ProductId=2,ProductName="Keyboard",Price=2_000
+                },
+                new Product()
+                {
+                    ProductId=3,ProductName="Android Watch",Price=12_000
+                },
+                new Product()
+                {
+                    ProductId=4,ProductName="Play Station",Price= 22_500
+                },
+                new Product()
+                {
+                    ProductId=5,ProductName="Book",Price=20
+                }
+            );
+        }
     }
 }
